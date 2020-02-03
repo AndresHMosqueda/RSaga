0d2e98f2eac6bd4822d1cb848f19d1ea0e94d390
AndresHMosqueda <andreshm94@gmail.com>
2020-02-02 18:02:57 -0600

    Connecting ImgGrid with Redux

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+//Root reducer to combine all reducers
+import { combineReducers } from 'redux';
+
+import loadingReducer from './loadingReducer';
+import imagesReducer from './imagesReducer';
+import errorReducer from './errorReducer';
+
+const rootReducer = combineReducers({
+    isLoading: loadingReducer,
+    images: imagesReducer,
+    error: errorReducer,
+});
+
+export default rootReducer;
